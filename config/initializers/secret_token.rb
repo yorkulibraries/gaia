aa7d15a2cdeeba47eb166a5254449c90b1330765
Ali Sadaqain YorkU <sadaqain@yorku.ca>
2022-04-05 12:15:38 -0400

    Fix find_assets error on production and remove secret_token.rb file

--- config/initializers/secret_token.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Gaia::Application.config.secret_token = 'ff5b4f4dd22502aef8a8b084c5dda2fb209702c8f957dfb1954fdb1a5fa1eb321df4b7a2daa8adbedae33c1036b6fe848845557506c92525d96c2d60e16f786b'
-Gaia::Application.config.secret_key_base = "9b578033c5bea781f1504e7c14759ad8cb18b304e0436abc1e7d4677eb71bdca9fc9b68f233e9b43e031809c7871f5f987fb9289200d06d6918ea686e2931498"
