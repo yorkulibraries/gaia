dd63378544097420b66a83da6661f26ee0633f66
Tuan Nguyen <amtuannguyen@gmail.com>
2024-03-26 09:20:35 -0400

    change fixture_file_upload to Rack::Test::UploadedFile.new

--- test/factories/attachments.rb
+++ test/factories/attachments.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
   factory :attachment do
      name { generate(:attachment_name) }
      description { nil }
-     file { fixture_file_upload( Rails.root + 'test/fixtures/test_pdf.pdf','application/pdf') }
+     file { Rack::Test::UploadedFile.new( Rails.root + 'test/fixtures/test_pdf.pdf','application/pdf') }
      association :data_request, factory: :data_request
      association :user, factory: :user
   end
