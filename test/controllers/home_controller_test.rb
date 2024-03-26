0e6e254ac43086f3215cb21adc11e81dbf038166
Tuan Nguyen <amtuannguyen@gmail.com>
2024-03-26 09:31:36 -0400

    fix wrong number of arg error in test

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -9,7 +9,8 @@ class HomeControllerTest < ActionController::TestCase
     end
 
     should "redirect to data requests url if Staff or MANAGER" do
-      @user.update(:role, User::MANAGER_ROLE)
+      @user.role = User::MANAGER_ROLE
+      @user.save
       get :index, params: {}
       assert_response :redirect
       assert_redirected_to data_requests_url, "Goes to all data requests"
@@ -17,7 +18,8 @@ class HomeControllerTest < ActionController::TestCase
 
 
     should "redirect to data requests url if REGULAR USER" do
-      @user.update(:role, User::REGULAR_USER_ROLE)
+      @user.role = User::REGULAR_USER_ROLE
+      @user.save
       session[:terms_accepted] = true
       get :index, params: {}
       assert_response :redirect
